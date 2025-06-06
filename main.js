c36af481a822a4256818ab8709016f4783bbdcb2
Deepika Namburi <enigmaticdeepika@gmail.com>
2025-06-06 21:25:12 +0530

    Update and rename Main.js to main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+// Smooth scroll behavior
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", e => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href").substring(1);
+      document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+    });
+  });
+});
